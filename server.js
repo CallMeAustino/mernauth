2bac7a52ea61b8a7def484f9696e23dd8ee45008
Austin <austin.hokun.wong@gmail.com>
2020-10-06 16:22:38 -0400

    fixed validation

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const cors = require('cors');
 
 const app = express();
 // Bodyparser middleware
+app.use(cors()); 
 app.use(
   bodyParser.urlencoded({
     extended: false

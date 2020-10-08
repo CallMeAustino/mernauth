799976f227302cfeffd109bc114098bd295b89f1
Austin <austin.hokun.wong@gmail.com>
2020-10-08 14:39:03 -0400

    added picture preview

--- client/src/components/robotcreator/Robotcreator.js
+++ client/src/components/robotcreator/Robotcreator.js
@@ -4,19 +4,32 @@ import { connect } from "react-redux";
 import ImageUploader from 'react-images-upload';
 import ReactFileReader from 'react-file-reader';
 
+const VALID_FILETYPES = {
+    "image/png": true,
+    "image/jpeg": true,
+    "image/gif": true,
+    "image/bmp": true,
+    "image/jpg": true
+}
+  
+
 class Robotcreator extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {image: null, fileTooBig: false, invalidType: false, imageURL: null}
+        this.handleFileChange = this.handleFileChange.bind(this)
+
          this.state = { 
              picture: null,
+             fileTooBig: false, 
+             image: null, 
+             photoURL: null
             };
          this.onDrop = this.onDrop.bind(this);
     }
  
     componentDidUpdate() {
-        const image = document.getElementById('image-preview');
-        image.srcObject = this.state.picture;
     }
 
     onDrop(picture) {
@@ -26,24 +39,61 @@ class Robotcreator extends Component {
         });
     }
  
+    handleFileChange(e) {
+        const file = e.target.files[0];
+        if (file.size > 1000000) {
+          this.setState({fileTooBig: true, image: null, photoURL: null})
+          return false;
+       }
+
+       if (!VALID_FILETYPES[file.type]){
+         // Extra security in case the html accept gets bypassed.
+         this.setState({invalidType: true, image: null, photoURL: null})
+         return false
+       }
+
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+          this.setState({ image: file, photoURL: fileReader.result, fileTooBig: false, invalidType: false});
+        }
+        fileReader.readAsDataURL(file);
+    }
+
     render() {
 
         return (
-            <>
-            
-                <img id='image-preview'/>
-                <div style={{ verticalAlign: 'center' }}className="center">
-                    <ImageUploader
-                        withIcon={true}
-                        buttonText='Choose image'
-                        onChange={this.onDrop}
-                        imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                        maxFileSize={5242880}
+            <div className="char-creation-pane"> 
+            <h1>Image</h1>
+            <form className="char-form" onSubmit={this.passTheProps}>  
+                <div className="image-preview">
+                {this.state.photoURL ? <img src={this.state.photoURL}/> : <div className="image-placeholder"><span>Image Preview </span></div>}
+                </div> 
+                  <div className="char-form-row">
+                    <input className="char-form-image"
+                    type="file"  
+                    onChange={this.handleFileChange} 
+                    accept="image/png, image/jpeg, image/gif, image/bmp, image/jpg"
                     />
+                  <input type="submit" className="submit" value="Submit" disabled={!this.state.image} />
                 </div>
-            </>
+               
+
+                <ul className="error-zone">
+                  {this.state.fileTooBig && <li>File Size must be 500 kB or less</li>}
+                  {this.state.invalidType && <li>Png, Jpeg, Gif, and Bmp only</li>}
+                </ul>
+            </form>
+          </div>
         );
     }
 }
 
  export default connect()(Robotcreator);
+
+  {/* <ImageUploader
+    withIcon={true}
+    buttonText='Choose image'
+    onChange={this.onDrop}
+    imgExtension={['.jpg', '.gif', '.png', '.gif']}
+    maxFileSize={5242880}
+    /> */}
